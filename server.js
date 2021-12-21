97b810b2aba9042c28a20bfad826851b93e09a8c
lisab1993 <brown.lisa999@gmail.com>
2021-12-21 13:51:06 -0800

    deploying

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = process.env.PORT || 8000
 
-app.listen(port, function() {
-    console.log('server listening on 8000')
+app.use(express.static(path.resolve(__dirname, "./evenoddapp/build")));
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}!`)
 })
